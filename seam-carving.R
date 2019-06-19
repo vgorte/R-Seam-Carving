8cc5a5cff05861df65cb602575aac4739fd14d99
Viktor Gorte <v_gort01@uni-muenster.de>
2019-06-19 22:51:25 +0200

    removing single seam #UNFINISHED

--- seam-carving.R
+++ seam-carving.R
@@ -4,16 +4,18 @@
 #if (!requireNamespace("BiocManager", quietly = TRUE))
 #install.packages("BiocManager")
 #BiocManager::install("EBImage")
+#DOCS: browseVignettes("EBImage")
 
 
 
 library(EBImage)
 
+#working
 calc_energy <- function(img) {
   print(img, short = T)
   
   # define horizontal and vertical Sobel kernel
-  kernal_h <- matrix(c(1, 2, 1, 0, 0, 0, -1, -2, -1), nrow = 3)
+  kernal_h <- matrix(c(1, 2, 1, 0, 0, 0,-1,-2,-1), nrow = 3)
   kernal_v <- t(kernal_h)
   
   # get horizontal and vertical edges
@@ -23,56 +25,95 @@ calc_energy <- function(img) {
   # combine edge pixel data to get overall edge data
   hdata <- imageData(imgH)
   vdata <- imageData(imgV)
-  edata <- sqrt(hdata^2 + vdata^2)
+  edata <- sqrt(hdata ^ 2 + vdata ^ 2)
   
   # transform edge data to image
   imgE <- Image(edata, colormode = 2)
   
-  print(display(combine(img, imgH, imgV, imgE), method = "raster", all = T))
+  print(display(combine(img, imgH, imgV, imgE),method = "raster",all = T))
   
-  imgE_summed <- (imgE@.Data[,,1] + imgE@.Data[,,2] + imgE@.Data[,,3]) / 3
+  imgE_summed <-(imgE@.Data[, , 1] + imgE@.Data[, , 2] + imgE@.Data[, , 3]) / 3
   return(imgE_summed)
 }
 
 
-minimum_seam <- function(img) {
-  ncol <- dim(img)[1]
-  nrow <- dim(img)[2]
-  energy_map <- calc_energy(img)
-  
-  M <- energy_map
-  backtrack <- energy_map *0L
 
-  for (i in c(1:ncol)) { #First row stays zero
-    for (j in  c(2:nrow)) {
-      if (j == 1) {
-        idx = min(c(M[i-1, j], M[i-1, j+1], M[i-1, j+2]))
-        backtrack[i,j] = idx + j
-        min_energy = M[i-1, idx+j]
-      }else if(j == ncol-1){
-        print("hi")
-        idx = min(c(M[i-1, j-1], M[i-1, j], M[i-1, j+1]))
-        backtrack[i,j] = idx + j - 1
-        min_energy = M[i-1, idx+j-1]
-      }else if(j == ncol){
-        print("yo")
-        idx = min(c(M[i-1, j-1], M[i-1, j]))
-        backtrack[i,j] = idx + j - 1
-        min_energy = M[i-1, idx+j-1]
-      }else{
-        idx = min(c(M[i-1, j-1], M[i-1, j], M[i-1, j+1], M[i-1, j+2]))
-        backtrack[i,j] = idx + j - 1
-        min_energy = M[i-1, idx+j-1]
-        return(which(M == idx, arr.ind = TRUE))
+
+minimum_seam <- function(img, ncol, nrow) {
+  energy_map <- calc_energy(img)
+  M <- energy_map
+  backtrack <- energy_map * 0L
+  
+  for (c in c(1:nrow)) {
+    for (r in c(2:ncol)) {
+      
+      
+      
+      if (c == 1) {
+        idx = which.min(c(M[c, r - 1], M[c + 1, r - 1]))
+        backtrack[c, r] = idx + c - 1
+        min_energy = M[idx + c - 1, r - 1]
+      } else if (c == ncol) {
+        idx = which.min(c(M[c - 1, r - 1], M[c, r - 1]))
+        backtrack[c, r] = c + idx - 2
+        min_energy = M[c + idx - 2, r - 1]
+      } else{
+        idx = which.min(c(M[c - 1, r - 1], M[c, r - 1], M[c + 1, r - 1]))
+        backtrack[c, r] = c + idx - 2
+        min_energy = M[c + idx - 2, r - 1]
       }
-      M[i,j] = M[i,j] + min_energy
     }
   }
+  min_energy
   
-  result <- list(M, backtrack)
+  
+  M[c, r] = M[c, r] + min_energy
+  result <- list(M = M, backtrack = backtrack)
   return(result)
 }
 
 
+
+
+carve_column <- function(img , ncol, nrow) {
+  imgEMap <- minimum_seam(img, ncol, nrow)
+  M <- imgEMap$M
+  backtrack <- imgEMap$backtrack
+  
+  #Creating a (c,r) matrix filled with the value TRUE
+  #All pixels that need to be removed will be marked as FALSE in the mask
+  mask <- matrix(TRUE, ncol = ncol, nrow = nrow,)
+  
+  #Position of smallest element in the last row of M
+  ielem <- which.min(imgEMap$M[, nrow])
+  
+  for (r in rev(c(1:nrow))) {
+    mask[r, ielem] = FALSE
+    ielem = backtrack[ielem, r]
+  }
+  
+  return(mask)
+}
+
+
+
 img <- readImage("testimg.jpg")
-imgEMap <- minimum_seam(img)
+ncol <- dim(img)[1]
+nrow <- dim(img)[2]
+
+mask <- carve_column(img, ncol, nrow)
+
+#length(which(mask)) #Number of TRUE's in mask -> Should be (ncol * nrow) - nrow
+
+
+r <- img[, , 1]
+g <- img[, , 2]
+b <- img[, , 3]
+
+
+r_masked <- Image(matrix(r[mask], nrow = nrow, ncol = ncol - 1))
+g_masked <- Image(matrix(g[mask], nrow = nrow, ncol = ncol - 1))
+b_masked <- Image(matrix(b[mask], nrow = nrow, ncol = ncol - 1))
+
+test_masked <-rgbImage(red = r_masked, green = g_masked, blue = b_masked)
+display(test_masked)
