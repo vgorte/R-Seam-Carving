27f31711224abf98c66e5d8e41283174552353d0
Viktor Gorte <v_gort01@uni-muenster.de>
2019-07-01 15:45:44 +0200

    SCImage-Class
    sc_reduce() returns SCImage class object with  energy map and reduced image

--- R/seam-carving.R
+++ R/seam-carving.R
@@ -236,18 +236,22 @@ checkUserInput <- function(img, ncols, nrows) {
 #' @param imgPath Path to a three channel (RGB) image. (.PNG / .JPG / .TIF)
 #' @param ncols Number by which the width has to be decreased
 #' @param nrows Number by which the height has to be decreased
-#' @return Image <EBImage class>
+#' @return SCImage <EnergyMap & Resized Image>
 #' @keywords seamcarving
 #' @export
 #' @examples
 #' library(EBImage)
 #' filePath <- system.file("images", "sample-color.png", package="EBImage")
-#' reducedImage <- sc_reduce(filePath, ncols = 1, nrows = 3)
-#' display(reducedImage)
+#' reducedImage <- sc_reduce(filePath, ncols = 1, nrows = 1)
+#' display(reducedImage$reduced_img)
+#' display(reducedImage$energy_map)
+
 sc_reduce <- function(imgPath, ncols = 0, nrows = 0) {
   img <- parseImage(imgPath)
   checkUserInput(img, ncols, nrows)
+  eMap <- calc_energy(img)
   img_reduced_r <- remove_rows(img, nrows)
   img_reduced <- remove_columns(img_reduced_r, ncols)
-  return(img_reduced)
+  result <- structure( list(energy_map = eMap, reduced_img = img_reduced), class="SCIMAGE")
+  return(result)
 }
